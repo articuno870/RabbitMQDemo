da3f43a8696079e9d8f802f2eecb33a6c7fb62ce
articuno870 <abhishek.prasad871@gmail.com>
2022-04-25 23:50:39 +0530

    package structure change

--- Consumer/src/main/java/com/example/demo/config/MessagingConfig.java
+++ Consumer/src/main/java/com/example/demo/config/MessagingConfig.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.config;
 
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
--- Consumer/src/main/java/com/example/demo/service/ConsumerListner.java
+++ Consumer/src/main/java/com/example/demo/service/ConsumerListner.java
@@ -1,7 +1,10 @@
-package com.example.demo;
+package com.example.demo.service;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
+
+import com.example.demo.config.MessagingConfig;
+import com.example.demo.model.OrderStatus;
 
 @Service
 public class ConsumerListner {
